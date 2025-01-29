806343dd75c340ca92a8aea333e5a0ffdb527670
AlexMonDev <ialexmonzon@gmail.com>
2025-01-29 21:23:40 +0100

    Initial commit

---
+++ POO_Ejemplo_2/Cliente.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_Ejemplo_2
+{
+    //  Entidad
+    public class Cliente
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+    }
+
+    public class ActividadesDelCLiente
+    {
+        List<Cliente> BaseDeDatos = new List<Cliente>();
+
+        public bool AltaDeCliente(Cliente cliente)
+        {
+            bool estado = false;
+
+            if(cliente != null)
+            {
+                BaseDeDatos.Add(cliente);
+                estado = true;
+            }
+            return estado;
+        }
+    }
+}
---
+++ POO_Ejemplo_2/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using POO_Ejemplo_2;
+
+main();
+void main()
+{
+    Cliente cliente1 = new Cliente();
+    cliente1.ID= 1;
+    cliente1.Nombre = "Juan";
+    cliente1.Apellido = "lopez";
+
+    Cliente cliente2 = new Cliente();
+    cliente2.ID = 2;
+    cliente2.Nombre = "Ana";
+    cliente2.Apellido = "Peralta";
+
+    ActividadesDelCLiente actividadesDelCLiente = new ActividadesDelCLiente();
+    actividadesDelCLiente.AltaDeCliente(cliente1);
+    actividadesDelCLiente.AltaDeCliente(cliente2);
+}
